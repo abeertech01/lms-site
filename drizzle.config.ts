99b61762a4be08a37a78a5344d2cb6abaa6ee68e
abeertech01 <abeer.technology@gmail.com>
2025-07-30 22:39:14 +0600

    clerk auth setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   },
 })
 /** port
- * setting port is necessary.
+ * setting port is necessary if you don't want the default port.
  * if you don't set a port postgres will use 5432 as default.
  * when I set "5433:5432" in compose file, it gave me error, because I didn't set port in drizzle.config.ts file. so, it set port 5432 as default. so, it mismatched and got us error.
  * port can be set in db.ts and drizzle.config.ts. But if I set in only one place, it has to be in drizzle.config.ts file.
--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -8,6 +8,7 @@ export const env = createEnv({
     DB_NAME: z.string().min(1),
     DB_HOST: z.string().min(1),
     DB_PORT: z.coerce.number().default(5432),
+    CLERK_SECRET_KEY: z.string().min(1),
   },
   experimental__runtimeEnv: process.env,
 })
