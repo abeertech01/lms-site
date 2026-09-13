d3924de426ccdd73d25a6f02d808b9882e601042
abeertech01 <abeer.technology@gmail.com>
2026-09-13 20:47:41 +0600

    little refactoring

--- src/app/(consumer)/layout.tsx
+++ src/app/(consumer)/layout.tsx
@@ -3,7 +3,7 @@ import { canAccessAdminPages } from "@/permissions/general"
 import { getCurrentUser } from "@/services/clerk"
 import { Show, SignInButton, UserButton } from "@clerk/nextjs"
 import Link from "next/link"
-import { ReactNode } from "react"
+import { ReactNode, Suspense } from "react"
 
 // TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
 // See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
@@ -39,7 +39,9 @@ function Navbar() {
             </Button>
           }
         >
-          <AdminLink />
+          <Suspense fallback={null}>
+            <AdminLink />
+          </Suspense>
           <Link
             href={"/courses"}
             className="hover:bg-accent/10 flex items-center px-2"
